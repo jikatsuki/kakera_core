373a6bde6fdebb626c0814a3d3d6e15cd891c10a
Drawoceans <aloneing0420@gmail.com>
2018-10-20 00:13:50 +0800

    change element APIs

--- include/kakera_element.h
+++ include/kakera_element.h
@@ -33,17 +33,22 @@ typedef void(*kakera_ElementEventCallback)(kakera_Element*);
 extern KAKERA_EXPORT kakera_Element* kakera_CreateElement(const char* id);
 extern KAKERA_EXPORT void kakera_DestroyElement(kakera_Element* element);
 extern KAKERA_EXPORT void kakera_SetElementDisplaySize(kakera_Element* element, int w, int h);
+extern KAKERA_EXPORT void kakera_GetElementDisplaySize(kakera_Element* element, int* w, int* h);
 extern KAKERA_EXPORT void kakera_SetElementRealSize(kakera_Element* element, int w, int h);
+extern KAKERA_EXPORT void kakera_GetElementRealSize(kakera_Element* element, int* w, int* h);
 extern KAKERA_EXPORT void kakera_SetElementPosition(kakera_Element* element, int x, int y);
+extern KAKERA_EXPORT void kakera_GetElementPosition(kakera_Element* element, int* x, int* y);
 extern KAKERA_EXPORT void kakera_MoveElementViewport(kakera_Element* element, int x, int y);
 extern KAKERA_EXPORT const char* kakera_GetElementName(kakera_Element* element);
 extern KAKERA_EXPORT void kakera_SetIsElementResponseEvent(kakera_Element* element, bool response);
 extern KAKERA_EXPORT void kakera_BindEvedntToElement(kakera_Element* element, kakera_ElementEvents event, kakera_ElementEventCallback callback);
-extern KAKERA_EXPORT void kakera_SetElementContentAsColor(kakera_Element* element, uint8_t r, uint8_t g, uint8_t b);
-extern KAKERA_EXPORT void kakera_SetElementContentAsPicture(kakera_Element* element, const char* picture);
-extern KAKERA_EXPORT void kakera_SetElementContentAsText(kakera_Element* element, const char* font, int size, uint8_t r, uint8_t g, uint8_t b, int style, const char* text);
-extern KAKERA_EXPORT void kakera_SetElementContentWithRawPixels(kakera_Element* element, kakera_PixelFormats format, bool isStatic, void* pixels);
+extern KAKERA_EXPORT char* kakera_GetPixelsFromColor(int w, int h, uint8_t r, uint8_t g, uint8_t b);
+extern KAKERA_EXPORT char* kakera_GetPixelsFromPicture(const char* picture);
+extern KAKERA_EXPORT char* kakera_GetPixelsFromText(const char* font, int size, uint8_t r, uint8_t g, uint8_t b, int style, const char* text, int* finalW, int* finalH);
+extern KAKERA_EXPORT void kakera_SetElementContentComplex(kakera_Element* element, kakera_PixelFormats format, bool isStatic, void* pixels);
 extern KAKERA_EXPORT void kakera_SetElementOpacity(kakera_Element* element, uint8_t opacity);
+
+#define kakera_SetElementContent(element,pixels) kakera_SetElementContentComplex(element, PIXEL_FORMAT_RGBA8888, true, pixels)
 
 #ifdef __cplusplus
 }
--- src/kakera_element.cpp
+++ src/kakera_element.cpp
@@ -1,3 +1,7 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif //!_MSC_VER
+
 #include "kakera_header.h"
 #include "kakera_element.h"
 #include "kakera_part_implementation.h"
@@ -27,16 +31,34 @@ void kakera_SetElementDisplaySize(kakera_Element* element, int w, int h)
     element->displaySize.h = h;
 }
 
+void kakera_GetElementDisplaySize(kakera_Element * element, int * w, int * h)
+{
+    *w = element->displaySize.w;
+    *h = element->displaySize.h;
+}
+
 void kakera_SetElementRealSize(kakera_Element* element, int w, int h)
 {
     element->realSize.w = w;
     element->realSize.h = h;
 }
 
+void kakera_GetElementRealSize(kakera_Element * element, int * w, int * h)
+{
+    *w = element->realSize.w;
+    *h = element->realSize.h;
+}
+
 void kakera_SetElementPosition(kakera_Element* element, int x, int y)
 {
     element->position.x = x;
     element->position.y = y;
+}
+
+void kakera_GetElementPosition(kakera_Element * element, int * x, int * y)
+{
+    *x = element->position.x;
+    *y = element->position.y;
 }
 
 void kakera_MoveElementViewport(kakera_Element* element, int x, int y)
@@ -60,36 +82,43 @@ void kakera_BindEvedntToElement(kakera_Element * element, kakera_ElementEvents e
     element->callbackList.emplace(event, callback);
 }
 
-void kakera_SetElementContentAsColor(kakera_Element * element, uint8_t r, uint8_t g, uint8_t b)
+char * kakera_GetPixelsFromColor(int w, int h, uint8_t r, uint8_t g, uint8_t b)
 {
-    SDL_Surface* surface = SDL_CreateRGBSurface(0, element->realSize.w, element->realSize.h, 32, 0, 0, 0, 0);
+    SDL_Surface* surface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
     SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, r, g, b));
-    element->texture = SDL_CreateTextureFromSurface(element->scene->window->renderer, surface);
-    SDL_SetTextureBlendMode(element->texture, SDL_BLENDMODE_BLEND);
+    int pixelSize = surface->pitch * surface->h;
+    char* result = new char[pixelSize];
+    memcpy(result, surface->pixels, pixelSize);
     SDL_FreeSurface(surface);
+    return result;
 }
 
-void kakera_SetElementContentAsPicture(kakera_Element * element, const char * picture)
+char * kakera_GetPixelsFromPicture(const char * picture)
 {
     SDL_Surface* surface = IMG_Load_RW(SDL_RWFromConstMem(picture, strlen(picture)), 1);
-    element->texture = SDL_CreateTextureFromSurface(element->scene->window->renderer, surface);
-    SDL_SetTextureBlendMode(element->texture, SDL_BLENDMODE_BLEND);
+    int pixelSize = surface->pitch * surface->h;
+    char* result = new char[pixelSize];
+    memcpy(result, surface->pixels, pixelSize);
     SDL_FreeSurface(surface);
+    return result;
 }
 
-void kakera_SetElementContentAsText(kakera_Element * element, const char * font, int size, uint8_t r, uint8_t g, uint8_t b, int style, const char * text)
+char * kakera_GetPixelsFromText(const char * font, int size, uint8_t r, uint8_t g, uint8_t b, int style, const char * text, int* finalW, int* finalH)
 {
     TTF_Font* SDLFont = TTF_OpenFontRW(SDL_RWFromConstMem(font, strlen(font)), 1, size);
     TTF_SetFontStyle(SDLFont, style);
     SDL_Surface* surface = TTF_RenderUTF8_Blended(SDLFont, text, { r, g, b });
     TTF_CloseFont(SDLFont);
-    kakera_SetElementRealSize(element, surface->w, surface->h);
-    element->texture = SDL_CreateTextureFromSurface(element->scene->window->renderer, surface);
-    SDL_SetTextureBlendMode(element->texture, SDL_BLENDMODE_BLEND);
+    int pixelSize = surface->pitch * surface->h;
+    char* result = new char[pixelSize];
+    memcpy(result, surface->pixels, pixelSize);
+    *finalW = surface->w;
+    *finalH = surface->h;
     SDL_FreeSurface(surface);
+    return result;
 }
 
-void kakera_SetElementContentWithRawPixels(kakera_Element* element, kakera_PixelFormats format, bool isStatic, void* pixels)
+void kakera_SetElementContentComplex(kakera_Element* element, kakera_PixelFormats format, bool isStatic, void* pixels)
 {
     int SDLFormat;
     if (format == PIXEL_FORMAT_RGBA8888)
